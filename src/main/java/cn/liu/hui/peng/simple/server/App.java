01680cffbd974e586528fac3beb33313c2a9866d
hz16092620 <liuhuipeng@huize.com>
2018-10-25 11:23:51 +0800

    init simple server.

---
+++ src/main/java/cn/liu/hui/peng/simple/server/App.java
@@ -0,0 +1,13 @@
+package cn.liu.hui.peng.simple.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
