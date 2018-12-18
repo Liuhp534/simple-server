13281f5e866ac96590210f82a237548cb55ea139
hz16092620 <liuhuipeng@huize.com>
2018-12-18 12:42:57 +0800

    分支添加一 2018年12月18日12:42:54。

---
+++ src/main/java/cn/liu/hui/peng/simple/server/BranchTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2006-2015 Hzins Ltd. All Rights Reserved. 
+ *  
+ * This code is the confidential and proprietary information of   
+ * Hzins. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with Hzins,http://www.hzins.com.
+ *  
+ */   
+package cn.liu.hui.peng.simple.server; 
+
+/**
+ * <p>
+ * 
+ *
+ *
+ * </p>
+ * @author	hz16092620 
+ * @date	2018年12月18日 下午12:42:18
+ * @version      
+ */
+public class BranchTest {
+
+    public static void main(String[] args) {
+	System.out.println("分支添加一 2018年12月18日12:42:34");
+    }
+}
+ 
