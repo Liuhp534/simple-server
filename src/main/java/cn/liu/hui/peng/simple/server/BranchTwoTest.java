6d00dff064c4e2f783d1f9ee1fdcde26b707cb25
hz16092620 <liuhuipeng@huize.com>
2018-12-18 13:59:55 +0800

    合并分支

---
+++ src/main/java/cn/liu/hui/peng/simple/server/BranchTwoTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2006-2015 Hzins Ltd. All Rights Reserved. 
+ *  
+ * This code is the confidential and proprietary information of   
+ * Hzins. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with Hzins,http://www.hzins.com.
+ *  
+ */   
+package cn.liu.hui.peng.simple.server; 
+
+/**
+ * <p>
+ * 
+ *
+ *
+ * </p>
+ * @author	hz16092620 
+ * @date	2018年12月18日 下午1:54:23
+ * @version      
+ */
+public class BranchTwoTest {
+
+    public static void main(String[] args) {
+	System.out.println("分支添加一");
+    }
+}
+ 
