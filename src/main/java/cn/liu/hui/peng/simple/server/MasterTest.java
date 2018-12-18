fb64e5c8de7d51f0746a2b2638bc85b3fa43bd7b
hz16092620 <liuhuipeng@huize.com>
2018-12-18 13:08:06 +0800

    合并master.

---
+++ src/main/java/cn/liu/hui/peng/simple/server/MasterTest.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2006-2015 Hzins Ltd. All Rights Reserved. 
+ *  
+ * This code is the confidential and proprietary information of   
+ * Hzins. You shall not disclose such Confidential Information   
+ * and shall use it only in accordance with the terms of the agreements   
+ * you entered into with Hzins,http://www.hzins.com.
+ *  
+ */   
+package cn.liu.hui.peng.simple.server; 
+
+/**
+ * <p>
+ * 
+ *
+ *
+ * </p>
+ * @author	hz16092620 
+ * @date	2018年12月18日 下午1:05:46
+ * @version      
+ */
+public class MasterTest {
+
+    public static void main(String[] args) {
+	System.out.println("master 添加一 2018年12月18日13:06:02");
+    }
+}
+ 
