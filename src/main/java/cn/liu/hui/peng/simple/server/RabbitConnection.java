ac6a7719a87de413f2a50b93a09ed83ec59791df
hz16092620 <liuhuipeng@huize.com>
2018-12-20 15:41:23 +0800

    去除标识。

--- src/main/java/cn/liu/hui/peng/simple/server/RabbitConnection.java
+++ src/main/java/cn/liu/hui/peng/simple/server/RabbitConnection.java
@@ -1,12 +1,3 @@
-/**
- * Copyright (c) 2006-2015 Hzins Ltd. All Rights Reserved. 
- *  
- * This code is the confidential and proprietary information of   
- * Hzins. You shall not disclose such Confidential Information   
- * and shall use it only in accordance with the terms of the agreements   
- * you entered into with Hzins,http://www.hzins.com.
- *  
- */   
 package cn.liu.hui.peng.simple.server; 
 
 import java.io.IOException;
