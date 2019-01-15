2772ee5038d0a270cf9359965bc1e81ecedef118
hz16092620 <liuhuipeng@huize.com>
2019-01-15 22:26:38 +0800

    阻塞socket和serversocket

---
+++ src/main/java/cn/liu/hui/peng/simple/server/socket/client/ClientSocketCustome.java
@@ -0,0 +1,48 @@
+package cn.liu.hui.peng.simple.server.socket.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @description: 客户端socket
+ * @author: hz16092620
+ * @create: 2019-01-15 17:57
+ */
+public class ClientSocketCustome {
+
+    public static void main(String[] args) {
+        clientCreate();
+    }
+
+    /**
+    * @Description: socket客户端
+    * @param
+    * @return
+    * @throws
+    * @author hz16092620
+    * @date 2019/1/15 22:23
+    */
+    private static void clientCreate() {
+        String data = "client data";
+        try {
+            Socket socket = new Socket("127.0.0.1", 8080);
+            //写数据
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.println(data);
+            writer.flush();
+
+            //读数据
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("server response data : " + reader.readLine());
+
+            writer.close();
+            reader.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/cn/liu/hui/peng/simple/server/socket/server/ServerSocketCustome.java
@@ -0,0 +1,52 @@
+package cn.liu.hui.peng.simple.server.socket.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @description: java服务端的socket
+ * @author: hz16092620
+ * @create: 2019-01-15 13:12
+ */
+public class ServerSocketCustome {
+
+    public static void main(String[] args) {
+        createServerSocket();
+    }
+
+    /**
+    * @Description: 创建服务端socket
+    * @param
+    * @return
+    * @throws
+    * @author hz16092620
+    * @date 2019/1/15 13:36
+    */
+    private static void createServerSocket() {
+        try {
+            //创建服务socket
+            ServerSocket serverSocket = new ServerSocket(8080);
+            //等待请求，阻塞的
+            Socket clientSocket = serverSocket.accept();
+            //接受数据
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String line = reader.readLine();
+            System.out.println("received from cilent : " + line);
+            //响应数据
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+            writer.write("received data : " + line);
+            writer.flush();
+            //关闭资源
+            reader.close();
+            writer.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
