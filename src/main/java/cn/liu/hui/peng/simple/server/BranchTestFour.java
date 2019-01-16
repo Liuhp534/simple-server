c9fe06571db619b00e09da67f6ad3c4e5caeaa26
hz16092620 <liuhuipeng@huize.com>
2019-01-16 18:07:29 +0800

    分支五提交2。

---
+++ src/main/java/cn/liu/hui/peng/simple/server/BranchTestFour.java
@@ -0,0 +1,13 @@
+package cn.liu.hui.peng.simple.server;
+
+/**
+ * @description: test
+ * @author: hz16092620
+ * @create: 2019-01-16 18:06
+ */
+public class BranchTestFour {
+
+    public static void main(String[] args) {
+        System.out.println("test.....");
+    }
+}
