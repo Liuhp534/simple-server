cd1b64d68ccc6f15e1b5db0c72c4aaaa707a225c
刘辉鹏 <liuhuipeng@huize.com>
2019-03-11 16:13:17 +0800

    主干提交1.git reset --soft

--- src/main/java/cn/liu/hui/peng/simple/server/BranchTestFour.java
+++ src/main/java/cn/liu/hui/peng/simple/server/BranchTestFour.java
@@ -35,5 +35,6 @@ public class BranchTestFour {
         System.out.println("2019年3月9日09:56:44");
         System.out.println("主干 2019年3月11日12:49:18");
         System.out.println("分支 2019年3月11日12:56:15");
+        System.out.println("主干 2019年3月11日16:12:47");
     }
 }
