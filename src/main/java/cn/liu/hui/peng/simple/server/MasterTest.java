abf2c85ccc25daa8476b8b78b45026d3b294d875
刘辉鹏 <liuhuipeng@huize.com>
2019-03-11 17:01:37 +0800

    主干提交1.git reset

--- src/main/java/cn/liu/hui/peng/simple/server/MasterTest.java
+++ src/main/java/cn/liu/hui/peng/simple/server/MasterTest.java
@@ -15,6 +15,7 @@ public class MasterTest {
     public static void main(String[] args) {
 
         System.out.println("master 添加一 2018年12月18日13:06:02  aaa");
+        System.out.println("master 2019年3月11日17:01:16");
     }
 }
  
