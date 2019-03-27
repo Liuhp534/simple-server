e13b1bb12076d4c83d92671a6be6193d00e0df26
刘辉鹏 <liuhuipeng@huize.com>
2019-03-27 18:17:29 +0800

    测试git reset 第二次提交。

---
+++ src/main/java/cn/liu/hui/peng/simple/server/GitResetFour.java
@@ -0,0 +1,12 @@
+package cn.liu.hui.peng.simple.server;
+
+/**
+ * @description: test
+ * @author: hz16092620
+ * @create: 2019-03-27 18:14
+ */
+public class GitResetFour {
+    public static void main(String[] args) {
+        System.out.println("第二次提交" + "2019年3月27日18:15:12");
+    }
+}
