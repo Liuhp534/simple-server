bd76480a529e4452226d0a5df0dd60d69238c997
刘辉鹏 <liuhuipeng@huize.com>
2019-03-27 18:05:15 +0800

    测试git reset 第二次提交的保留，第一次的回滚需要

---
+++ src/main/java/cn/liu/hui/peng/simple/server/GitResetTwo.java
@@ -0,0 +1,12 @@
+package cn.liu.hui.peng.simple.server;
+
+/**
+ * @description: 测试git reset 第二次提交的保留，第一次的回滚需要
+ * @author: hz16092620
+ * @create: 2019-03-27 18:04
+ */
+public class GitResetTwo {
+    public static void main(String[] args) {
+        System.out.println("第二次提交" + "2019年3月27日18:04:55");
+    }
+}
