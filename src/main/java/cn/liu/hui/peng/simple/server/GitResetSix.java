363bc039e6f23cab6374f7f20a1357cbd31e78b5
刘辉鹏 <liuhuipeng@huize.com>
2019-03-28 11:05:15 +0800

    测试git reset add。回滚GitFive。保留GitSix。reset mixed 之后force push。

---
+++ src/main/java/cn/liu/hui/peng/simple/server/GitResetSix.java
@@ -0,0 +1,12 @@
+package cn.liu.hui.peng.simple.server;
+
+/**
+ * @description: test
+ * @author: hz16092620
+ * @create: 2019-03-28 11:02
+ */
+public class GitResetSix {
+    public static void main(String[] args) {
+        System.out.println("reset add" + "2019年3月28日11:02:01");
+    }
+}
