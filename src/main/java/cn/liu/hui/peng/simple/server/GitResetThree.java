eb405af961f3695f1609bfb18c5eca67f4e6fc05
刘辉鹏 <liuhuipeng@huize.com>
2019-03-28 11:18:18 +0800

    测试git reset update。commit1 no force push.

--- src/main/java/cn/liu/hui/peng/simple/server/GitResetThree.java
+++ src/main/java/cn/liu/hui/peng/simple/server/GitResetThree.java
@@ -8,5 +8,6 @@ package cn.liu.hui.peng.simple.server;
 public class GitResetThree {
     public static void main(String[] args) {
         System.out.println("第一次提交" + "2019年3月27日18:14:06");
+        System.out.println("reset update 不force push");
     }
 }
