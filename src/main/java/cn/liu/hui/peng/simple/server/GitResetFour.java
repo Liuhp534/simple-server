0627276419c2ed9b5d1608b352f1b16833c6b34f
刘辉鹏 <liuhuipeng@huize.com>
2019-03-28 10:50:41 +0800

    测试git reset force push测试。

--- src/main/java/cn/liu/hui/peng/simple/server/GitResetFour.java
+++ src/main/java/cn/liu/hui/peng/simple/server/GitResetFour.java
@@ -8,5 +8,6 @@ package cn.liu.hui.peng.simple.server;
 public class GitResetFour {
     public static void main(String[] args) {
         System.out.println("第二次提交" + "2019年3月27日18:15:12");
+        System.out.println("第二次提交" + "2019年3月27日18:22:36");
     }
 }
